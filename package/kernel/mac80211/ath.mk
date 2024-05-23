0b3f2911da09b1418b6979ecaf3d3a3598338d71
doveki <2290218900@qq.com>
2024-05-23 18:53:27 +0800

    pbuf script:add stock layout variant for xiaomi ax3600/ax6

--- package/kernel/mac80211/ath.mk
+++ package/kernel/mac80211/ath.mk
@@ -383,7 +383,9 @@ define KernelPackage/ath11k/config
                	 TARGET_qualcommax_ipq807x_DEVICE_compex_wpq873 || \
                	 TARGET_qualcommax_ipq807x_DEVICE_linksys_mx4200v1 || \
                	 TARGET_qualcommax_ipq807x_DEVICE_redmi_ax6 || \
+               	 TARGET_qualcommax_ipq807x_DEVICE_redmi_ax6-stock || \
                	 TARGET_qualcommax_ipq807x_DEVICE_xiaomi_ax3600 || \
+               	 TARGET_qualcommax_ipq807x_DEVICE_xiaomi_ax3600-stock || \
                	 TARGET_qualcommax_ipq807x_DEVICE_zte_mf269 )
                select ATH11K_MEM_PROFILE_256M if TARGET_qualcommax_ipq807x_DEVICE_netgear_wax218
 
